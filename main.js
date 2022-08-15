0ab277336399a6263d1c8b4783a0c0a99711c69b
bassima_gh <ghba20wu@stident.ju.se>
2022-08-16 01:50:58 +0200

    fixed some issues

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
+let numberInputElement;
+let bodyE;
+
 function numberConditions() {
-    const numberInputElement = document.getElementById("numberInput");
+    numberInputElement = document.getElementById("numberInput");
+    numberInputElement.innerHTML = "";
     let numberInputElementValue = numberInputElement.value;
     const randomNumValue = Math.floor(Math.random() * 100);
 
     if (numberInputElementValue === "") {
         alert("Please enter a number");
-    } else if (numberInputElementValue !== "") {
+    } else if (100 < numberInputElementValue  || numberInputElementValue < 0) {
+        alert("Please enter a number between 0-100")      
+    } else if (numberInputElementValue !== "" && 100 >= numberInputElementValue >= 0) {
         let randomAns = document.createElement("p");
-        let anstext = document.createTextNode("Correct Number is " + randomNumValue);
-        randomAns.appendChild(anstext);
-        const bodyE = document.getElementById("randomeNumberElement");
+        let anstext = document.createTextNode("Your number is " + numberInputElementValue + " correct Number is " + randomNumValue);
+        let ansArry = [];
+
+        ansArry.push(anstext);
+        randomAns.appendChild(ansArry[0]);
+        bodyE = document.getElementById("randomeNumberElement");
         bodyE.appendChild(randomAns);
+
+        console.log(ansArry);      
     }
     
 
-    if (numberInputElementValue > randomNumValue) {
+    if (numberInputElementValue > randomNumValue && numberInputElementValue !== "" && 100 >= numberInputElementValue && numberInputElementValue >= 0) {
         alert("Number less than");
         console.log(randomNumValue);
-    } else if (numberInputElementValue < randomNumValue) {
+    } else if (numberInputElementValue < randomNumValue && numberInputElementValue !== "" && 100 >= numberInputElementValue && numberInputElementValue >= 0) {
         alert("Number greater than");
         console.log(randomNumValue);
-    }  else if (numberInputElementValue === randomNumValue) {
+    }  else if (numberInputElementValue === randomNumValue && numberInputElementValue !== "" && 100 >= numberInputElementValue && numberInputElementValue >= 0) {
         alert("Number equal");
         console.log(randomNumValue);
     }
@@ -30,6 +41,14 @@ function numberConditions() {
 function loadHandler() {
     const submitInputElement = document.getElementById("submitBtn");
     submitInputElement.addEventListener("click", numberConditions);
+
+    const resetBtnElement = document.getElementById("resetbtn");
+    resetBtnElement.addEventListener("click", function () {
+        let formElement = document.getElementById("formContainer");
+        formElement.reset();
+
+        bodyE.innerHTML = "";
+    })
 }
 
 
