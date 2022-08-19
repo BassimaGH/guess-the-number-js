a1201e5adddfba77f50398e99e8169a089144291
bassima_gh <ghba20wu@stident.ju.se>
2022-08-20 01:59:05 +0200

    solved most of the problem

--- main.js
+++ main.js
@@ -1,54 +1,75 @@
-let numberInputElement;
-let bodyE;
+let randomNumValue = Math.floor(Math.random() * 100);
+let guessTries = 1;
+let numberInputElementValue = document.getElementById("numberInput");
+
 
 function numberConditions() {
-    numberInputElement = document.getElementById("numberInput");
-    numberInputElement.innerHTML = "";
-    let numberInputElementValue = numberInputElement.value;
-    const randomNumValue = Math.floor(Math.random() * 100);
+    numberInputElementValue = document.getElementById("numberInput").value;
 
-    if (numberInputElementValue === "") {
-        alert("Please enter a number");
-    } else if (100 < numberInputElementValue  || numberInputElementValue < 0) {
-        alert("Please enter a number between 0-100")      
-    } else if (numberInputElementValue !== "" && 100 >= numberInputElementValue >= 0) {
-        let randomAns = document.createElement("p");
-        let anstext = document.createTextNode("Your number is " + numberInputElementValue + " correct Number is " + randomNumValue);
-        let ansArry = [];
-
-        ansArry.push(anstext);
-        randomAns.appendChild(ansArry[0]);
-        bodyE = document.getElementById("randomeNumberElement");
-        bodyE.appendChild(randomAns);
-
-        console.log(ansArry);      
-    }
-    
-
-    if (numberInputElementValue > randomNumValue && numberInputElementValue !== "" && 100 >= numberInputElementValue && numberInputElementValue >= 0) {
-        alert("Number less than");
-        console.log(randomNumValue);
-    } else if (numberInputElementValue < randomNumValue && numberInputElementValue !== "" && 100 >= numberInputElementValue && numberInputElementValue >= 0) {
-        alert("Number greater than");
-        console.log(randomNumValue);
-    }  else if (numberInputElementValue === randomNumValue && numberInputElementValue !== "" && 100 >= numberInputElementValue && numberInputElementValue >= 0) {
-        alert("Number equal");
-        console.log(randomNumValue);
+    if (numberInputElementValue == randomNumValue) {
+        wonGame();
+    } else if (guessTries === 5) {
+        lostGame();
+    } else {
+        if (numberInputElementValue > randomNumValue) {
+            alert("WRONG GUESS!! TRY A SMALLER NUMBER");
+            addAtemptToList();
+            console.log(randomNumValue);
+            console.log(guessTries);
+        } else if (numberInputElementValue < randomNumValue) {
+            alert("WRONG GUESS!! TRY A GREATER NUMBER");
+            addAtemptToList();
+            console.log(randomNumValue);
+            console.log(guessTries);
+        } 
     }
 
+    guessTries++;
 }
+
+function lostGame() {
+    alert("GAME OVER!! THE CORRECT ANSWER IS " + randomNumValue);
+    
+    resetGame();
+}
+
+function wonGame() {
+    alert("YOU GUESSED IT RIGHT!! YOU NEEDED " + guessTries + "GUESS");
+    console.log(randomNumValue);
+    console.log(guessTries);
+
+    resetGame();
+}
+
+function resetGame() {
+    guessTries = 1;
+    randomNumValue = Math.floor(Math.random() * 100);
+
+    let formElement = document.getElementById("formContainer");
+    formElement.reset();
+}
+
+function addAtemptToList() {
+    const atemptList = document.createElement("li");
+    const atemptListElements = document.createElement("p");
+    const atemptListContainer = document.getElementById("randomeNumberElement");
+
+    if (randomNumValue != numberInputElementValue.value) {
+        atemptListElements.innerText = numberInputElementValue.value;
+        atemptList.appendChild(atemptListElements);
+
+        atemptListContainer.appendChild(atemptList);
+
+    }
+} 
+
 
 function loadHandler() {
     const submitInputElement = document.getElementById("submitBtn");
     submitInputElement.addEventListener("click", numberConditions);
 
     const resetBtnElement = document.getElementById("resetbtn");
-    resetBtnElement.addEventListener("click", function () {
-        let formElement = document.getElementById("formContainer");
-        formElement.reset();
-
-        bodyE.innerHTML = "";
-    })
+    resetBtnElement.addEventListener("click", resetGame)
 }
 
 
